edb38a596d434b2658dc0bf5127d4ecfea4e2dc6
flapjackstan <elm.s.camargo@gmail.com>
2019-02-06 01:27:37 +0000

    initial hello world assignment

---
+++ HelloWorld.cpp
@@ -0,0 +1,22 @@
+/*
+A. Name: Elmer Camargo
+B. ID: 2291367
+C. Email: camargop@chapman.edu
+D. Course: CPSC 350; Section 2:
+E. Assignment: 1
+
+
+Prof. Rene German
+11:30 A.M. Section
+Date: 02/05/2019
+
+Assignment 1 Hello World!
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n"; 
+}
